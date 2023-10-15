aff791a9e636a4cc3713e470a3522aaf6742d20e
MilagrosLV <lopezvmilagros@gmail.com>
2023-10-15 19:04:30 -0300

    update

---
+++ library 2/conexion_be.php
@@ -0,0 +1,10 @@
+<?php 
+ $conexion = mysqli_connect("localhost","root","","registro_inicio");
+ 
+ if($conexion){
+    echo 'Conectado exitosamente a la Base de Datos';
+ }else{
+    echo 'no se ha podido conectar a la Base de Datos';
+ }
+ 
+?>
---
+++ library 2/login_be.php
@@ -0,0 +1,25 @@
+<?php 
+
+session_start();
+
+$Correo = $_POST['Correo'];
+$Contraseña =$_POST['Contraseña'];
+$conexion = mysqli_connect("localhost","root","","registro_inicio");
+
+$verificar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$Correo' and contraseña='$Contraseña'" );
+
+if(mysqli_num_rows($verificar_login) > 0){
+    $_SESSION['usuario'] = $correo;
+    header("location: bienbenido.php");
+    exit; 
+    } else{
+    echo '
+    <script>
+    alert("Los datos de usuarios no se registraron y intente otraves oregistrese en la pagina");
+    window.location = "index.php";
+    </script>
+    ';
+    exit;  
+}
+
+?>
