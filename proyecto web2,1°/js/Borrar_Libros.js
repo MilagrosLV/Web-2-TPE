f4b1a4027f5f3e9e9b3918c6fc51b20f49e7ff63
MilagrosLV <lopezvmilagros@gmail.com>
2023-10-17 23:05:09 -0300

    update

--- proyecto web2,1°/js/Borrar_Libros.js
+++
@@ -1,9 +0,0 @@
-function borrarLibro(button) {
-    var contenedor = button.parentElement; 
-
-    var confirmacion = confirm("¿Estás seguro de que deseas borrar este libro?");
-
-    if (confirmacion) {
-        contenedor.remove(); 
-    }
-}
